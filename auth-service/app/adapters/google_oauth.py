d9ee873f3f640b8021b8ec4f2011bbbcc10cc96b
Nizshime-Samara <samaramathildes@gmail.com>
2025-05-12 13:33:49 -0300

    Refatoração de client

--- auth-service/app/adapters/google_oauth.py
+++ auth-service/app/adapters/google_oauth.py
@@ -10,7 +10,8 @@ oauth.register(
     client_id=settings.GOOGLE_CLIENT_ID,
     client_secret=settings.GOOGLE_CLIENT_SECRET,
     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    client_kwargs={"scope": "openid email profile", "response_type": "code id_token"},
+    client_kwargs={"scope": "openid email profile", "response_type": "code id_token", "response_mode": "query",},
+    
     
     userinfo_endpoint=userinfo_endpoint  # <- se seu client suportar
 )
