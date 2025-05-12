8f886a2c5c7b7aed9c80f8f674f897e6b7dd603b
Nizshime-Samara <samaramathildes@gmail.com>
2025-05-12 00:25:32 -0300

    Correção de importaçao do BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import AuthCallback from './pages/AuthCallback';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <BrowserRouter basename="/plataforma-resenhas-livros">
+    <BrowserRouter  basename="/plataforma-resenhas-livros">
       <Routes>
         <Route path="/auth/callback" element={<AuthCallback />} />        
         <Route path="/login" element={<Login />} />
