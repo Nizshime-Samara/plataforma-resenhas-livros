649bec376b240c204cffa4ea49ff81763a45c8cd
Nizshime-Samara <samaramathildes@gmail.com>
2025-05-12 00:41:10 -0300

    Atualizando url de backend do login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Login = () => {
-  const backendUrl = 'http://172.19.27.43:8000';  // ou process.env.REACT_APP_API_BASE_URL
+  const backendUrl = 'https://plataforma-resenhas-livros.onrender.com';  // ou process.env.REACT_APP_API_BASE_URL
 
   const handleLogin = () => {
     window.location.href = `${backendUrl}/api/v1/auth/login/google`;
